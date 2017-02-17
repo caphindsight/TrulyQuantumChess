190652afe22642338d6a414bb87d2739b18562f6
Cap. Hindsight <hindsight@yandex.ru>
2017-02-18 00:48:06 +0300

    Temporarily disabled perform_measurements

--- kernel/engine/src/quantize.rs
+++ kernel/engine/src/quantize.rs
@@ -103,7 +103,6 @@ impl QuantumChessboard {
     /// Performs measurements for each square in a superposition of pieces
     pub fn perform_measurements(&mut self) {
         // TODO: implement this. This is IMPORTANT!
-        panic!("Not implemented yet!");
     }
 
     /// Groups together similar harmonics, adding degeneracies.
