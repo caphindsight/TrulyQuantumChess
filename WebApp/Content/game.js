1fad5c6d23038216437245d3a9a6f8cd444ac403
Cap. Hindsight <hindsight@yandex.ru>
2017-02-23 19:30:11 +0300

    Preparing to write web UI

---
+++ WebApp/Content/game.js
@@ -0,0 +1,8 @@
+﻿function initialize_chessboard(board) {
+    $("#game_state").val(board.gameState);
+    $("#active_player").val(board.activePlayer);
+    for (var pos in board.squares) {
+        var square = board.squares[pos];
+        // TODO: actually draw the chessboard
+    }
+}
