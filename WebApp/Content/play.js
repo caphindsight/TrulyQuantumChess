c8c499dfc05915edb4368ca7b74b09a7bea74f6f
Cap. Hindsight <hindsight@yandex.ru>
2017-02-24 16:07:07 +0300

    Add support for drawing quantum pieces

--- WebApp/Content/play.js
+++ WebApp/Content/play.js
@@ -1,7 +1,20 @@
-﻿function draw($square, data) {
-    $square.val("");
+﻿function draw(canvas, data) {
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     if (data != null) {
-        $square.html('<img src="content/pieces/' + data.player + '_' + data.piece + '_alive.png" width="100%" height="100%"></img>');
+        var img_alive = new Image();
+        img_alive.src = "/content/pieces/" + data.player + "_" + data.piece + "_alive.png";
+        img_alive.onload = function() {
+            ctx.drawImage(img_alive, 0, 0, img_alive.width * data.probability, img_alive.height,
+                0, 0, canvas.width * data.probability, canvas.height);
+        };
+
+        var img_dead = new Image();
+        img_dead.src = "/content/pieces/" + data.player + "_" + data.piece + "_dead.png";
+        img_dead.onload = function() {
+            ctx.drawImage(img_dead, img_dead.width * data.probability, 0, img_dead.width * (1.0 - data.probability), img_dead.height,
+                canvas.width * data.probability, 0, canvas.width * (1.0 - data.probability), canvas.height);
+        };
     }
 }
 
@@ -10,7 +23,7 @@ function update_chessboard() {
         $("#game_state").html(data.gameState);
         $("#active_player").html(data.activePlayer);
         for (var pos in data.squares)
-            draw($("#sq-" + pos), data.squares[pos]);
+            draw($("#sq-" + pos)[0], data.squares[pos]);
     });
 }
 
